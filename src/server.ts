4fa88cac35d35f08d4561eafd476e7f4c6603f0e
PedroNunes <162641504+PedroNunesc@users.noreply.github.com>
2025-11-30 16:49:52 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
 import express, { Application } from "express";
 import { AppDataSource } from "./config/data-source";
-import authRoutes from "./routes/AuthRoutes";
-import userRoutes from "./routes/UserRoutes";
-import tripRoutes from "./routes/TripRoutes";
+import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoutes";
+import tripRoutes from "./routes/tripRoutes";
 import itemRoutes from "./routes/ItemRoutes";
 import * as dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", tripRoutes);
-app.use("/api", itemRoutes)
+app.use("/api", itemRoutes);
 
 const PORT = Number(process.env.PORT || "3000");
 
