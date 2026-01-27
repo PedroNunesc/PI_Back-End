3ad18735924e339445a82ebf5acbf67152ec753b
PedroNunes <162641504+PedroNunesc@users.noreply.github.com>
2026-01-27 20:21:39 -0300

    Refactor route imports to use consistent casing

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express, { Application } from "express";
 import { AppDataSource } from "./config/data-source";
-import authRoutes from "./routes/authRoutes";
-import userRoutes from "./routes/userRoutes";
-import tripRoutes from "./routes/tripRoutes";
+import authRoutes from "./routes/AuthRoutes";
+import userRoutes from "./routes/UserRoutes";
+import tripRoutes from "./routes/TripRoutes";
 import itemRoutes from "./routes/ItemRoutes";
 import * as dotenv from "dotenv";
 import cors from "cors";
