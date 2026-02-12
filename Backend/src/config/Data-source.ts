0be0e9fe7f89de2b7564d9bdfbe1e15766d081ca
Pedro Nunes <pnunescarlos@gmail.com>
2026-02-12 16:54:19 -0300

    Update Data-source.ts

--- Backend/src/config/Data-source.ts
+++ Backend/src/config/Data-source.ts
@@ -1,24 +1,17 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-if (process.env.NODE_ENV !== "production") {
-  import("dotenv").then(dotenv => dotenv.config());
-}
+const { DATABASE_URL } = process.env;
 
-const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
-
-if (!DB_HOST || !DB_PORT || !DB_USERNAME || !DB_PASSWORD || !DB_NAME) {
-  throw new Error("Erro: Variáveis de ambiente do banco não definidas!");
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL não definida!");
 }
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: DB_HOST,
-    port: Number(DB_PORT),
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    synchronize: false,
-    logging: true,
-    entities: [__dirname + "/../models/*.ts"],
+  type: "postgres",
+  url: DATABASE_URL,         
+  synchronize: false,
+  logging: true,
+  entities: [__dirname + "/../models/*.ts"],
+  ssl: { rejectUnauthorized: false }, 
 });
