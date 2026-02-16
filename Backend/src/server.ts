8d47125b92577482f85781c5b66f082faee31378
Pedro Nunes <pnunescarlos@gmail.com>
2026-02-16 12:04:11 -0300

    Update server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -13,7 +13,12 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+    origin: [
+      "http://127.0.0.1:5500",
+      "http://localhost:5500",
+      "http://localhost:3000",
+      "https://pi-front-end-xfkk.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -27,7 +32,7 @@ app.use("/api", userRoutes);
 app.use("/api", tripRoutes);
 app.use("/api", itemRoutes);
 
-const PORT = Number(process.env.PORT || "3000");
+const PORT = Number(process.env.PORT || 3000);
 
 AppDataSource.initialize()
   .then(() => {
